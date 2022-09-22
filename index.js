01c198412e4f26febbf4738f7aa82cd234dd018d
18kj18 <112865193+18kj18@users.noreply.github.com>
2022-09-22 18:21:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,124 @@
+console.log("what is the dog doing") //this
+//single line comment
+
+/*
+this is a multi
+line comment
+it covers several lines
+*/
+
+//VARIABLES
+
+/*
+there are two ways to declare variables in javascript
+
+var a = 10;
+defines variable localy
+
+let a = 11;
+defines variable globaly
+
+= sign is an assignment
+
+const x = 11;
+constant; its value cannot change or it will crash
+*/
+
+const x = 1;
+console.log(x)
+
+let a = 10;
+console.log(a)
+
+//bob != Bob (case-sensetive)(the case really is sensetive)
+//semi-colons note needed but should use anyway
+
+//DATATYPES
+/*
+numbers
+strings
+booleans (true of false)
+*/
+
+//type: number
+let temp = 10
+
+//type: string
+//quotation marks = text
+let temp1 = "10"
+let temp11 = "hello"
+
+//all numbers in javascript are floating point numbers (real numbers)
+
+//all variable names in javascript must begin with a letter or and underscore
+//you cannot use a reserved word as a varaible name
+
+
+//LOS MATHEMATICAS (order of operations)
+//ADDITION
+//just incase you forget 
+console.log(5+2);
+
+console.log("abc"+"def");
+
+//SUBTRACTION
+console.log(5-6);
+
+//MULTIPLICATION
+
+console.log(4*5);
+
+//DIVISION
+
+console.log(20/5);
+
+//EXPONENTS(square numbers)
+
+console.log(5**2);
+
+//INCREMENT
+
+//post increment(increment after the statement)
+let h = 10;
+
+console.log("h");
+console.log(h++);
+console.log(h)
+console.log(h)
+
+//pre increment(increment, NOW)
+console.log("k")
+let k = 5;
+console.log(++k)
+console.log(k)
+
+//COMPARISON OPERATORS: these will return true or false
+
+//equals ==
+
+console.log(5 == 3);
+
+console.log(4 == 4);
+
+//not equals !=
+
+console.log(5!=4);
+
+console.log(4!=4);
+
+//greater than > and less than <
+
+console.log(5> 4);
+
+console.log(4 >5)
+
+console.log(5< 4);
+
+console.log(4<5);
+
+//greater than or equal to >= and less or equal <=
+
+console.log(5<= 4);
+
+console.log(4 <=5);
+
---
+++ mathsthing.js
@@ -0,0 +1,49 @@
+const PI = 3.14;
+
+//height
+let H = 15;
+
+//width
+let W = 20;
+
+//length
+let L = 10;
+
+//radius
+let R = 10;
+
+//question 1
+let AA = PI*R**2;
+let BB = 2*PI*R;
+
+//question 2
+let CC = L*W;
+
+//question 3
+let Ba = 20;
+let DD = 1/2*Ba*H;
+
+//question 4
+let C = 2;
+let A = 4;
+let B = 5;
+let EE = A**2+B**2-C**2;
+
+//question 5
+let FF = L*W*H;
+
+//question 6
+let GG = PI*(R**2)*H;
+
+//question 7
+let HH = 4/3*PI*R**3;
+
+//console log
+console.log(AA);
+console.log(BB);
+console.log(CC);
+console.log(DD);
+console.log(EE);
+console.log(FF);
+console.log(GG);
+console.log(HH);
